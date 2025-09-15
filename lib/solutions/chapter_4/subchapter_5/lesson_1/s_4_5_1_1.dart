8a644c80723abaf0ff891e8efd2e34ebe71ade77
Dominik Steindl <dominik.steindl@gmx.de>
2025-09-15 10:42:19 +0200

    implement solution for 4_5_1_1

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 class S4511 extends StatelessWidget {
   const S4511({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return HelloWorld();
+  }
+
+  
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello World");
   }
 }
