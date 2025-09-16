16f5895918aa89d2ede7cbd69ab1122926fe5bf5
Dominik Steindl <dominik.steindl@gmx.de>
2025-09-16 14:07:28 +0200

    implemented 4.5.3.5 and formatted code

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,10 +4,12 @@ class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(height: 100.0, width: 100.0, color: Colors.blueGrey),
-      Container(height: 100.0, width: 100.0, color: Colors.blue),
-      Container(height: 100.0, width: 100.0, color: Colors.cyan)
-    ],);
+    return Column(
+      children: [
+        Container(height: 100.0, width: 100.0, color: Colors.blueGrey),
+        Container(height: 100.0, width: 100.0, color: Colors.blue),
+        Container(height: 100.0, width: 100.0, color: Colors.cyan)
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,19 @@ class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(width: 100.0, height: 100.0, color: Colors.red),
+            Container(width: 100.0, height: 100.0, color: Colors.green)
+          ],
+        ),
+        Row(children: [
+          Container(width: 100.0, height: 100.0, color: Colors.blue),
+          Container(width: 100.0, height: 100.0, color: Colors.yellow)
+        ])
+      ],
+    );
   }
 }
