0a0564a17044dbb2fa988afebc5c8858fb6e09d4
Dominik Steindl <dominik.steindl@gmx.de>
2025-09-17 11:56:36 +0200

    implement 4.5.2.3 and 4.5.2.4

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,32 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  void _changeName() {
+    setState(() {
+      name = "Dominik";
+    });
+  }
+
+  String name = "";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(name),
+        ElevatedButton(onPressed: _changeName, child: Text("Name anzeigen"))
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,49 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
   }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String name = "";
+  String label = "Name anzeigen";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(name),
+        ElevatedButton(
+            onPressed: () => setState(() {
+                  if (name.isEmpty) {
+                    name = "Dominik";
+                    label = NameState.shown.label;
+                  } else {
+                    name = "";
+                    label = NameState.hidden.label;
+                  }
+                }),
+            child: Text(label))
+      ],
+    );
+  }
+}
+
+enum NameState {
+  shown("Name verstecken"),
+  hidden("Name anzeigen");
+
+  final String label;
+
+  const NameState(this.label);
+
+  @override
+  String toString() => label;
 }
