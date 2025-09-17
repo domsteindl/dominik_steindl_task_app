e77c289583c2ef7407b166a759dd6bd726d46464
Dominik Steindl <dominik.steindl@gmx.de>
2025-09-17 13:01:29 +0200

    implement 4.5.2.1

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,7 +4,18 @@ class S4521 extends StatelessWidget {
   const S4521({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyWidget();
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Hello World"),
+    );
   }
 }
 
