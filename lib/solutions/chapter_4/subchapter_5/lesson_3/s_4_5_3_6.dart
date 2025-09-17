6fc4eb7e670939c6942263212401b05db389eb00
Dominik Steindl <dominik.steindl@gmx.de>
2025-09-17 12:29:31 +0200

    implement 4.5.3.6

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 
+final List<Product> products = [
+  Product(name: "Apple", price: 0.99),
+  Product(name: "Banana", price: 0.50),
+  Product(name: "Orange", price: 0.75),
+  Product(name: "Mango", price: 1.20),
+  Product(name: "Pineapple", price: 2.00),
+];
+
 class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView(
+      shrinkWrap: true,
+        children: products
+            .map((product) => ListTile(
+                  title: Text(product.name),
+                  subtitle: Text("€${product.price.toStringAsFixed(2)}"),
+                ))
+            .toList());
   }
+}
+
+class Product {
+  String name;
+  double price;
+
+  Product({
+    required this.name,
+    required this.price,
+  });
 }
